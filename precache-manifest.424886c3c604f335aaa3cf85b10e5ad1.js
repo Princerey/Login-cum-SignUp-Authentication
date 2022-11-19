905a4376ed2f2f1345369c5a85a4dda9b11f8c49
Princerey <rohkumar0126@gmail.com>
2022-11-19 09:02:30 +0530

    Updates

---
+++ precache-manifest.424886c3c604f335aaa3cf85b10e5ad1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6d82a52a1975723a1adad21f6fccd7",
+    "url": "/Login-cum-SignUp-Authentication/index.html"
+  },
+  {
+    "revision": "634653f56002394b99f9",
+    "url": "/Login-cum-SignUp-Authentication/static/css/main.8557f35f.chunk.css"
+  },
+  {
+    "revision": "140e1f40f2794fa84efd",
+    "url": "/Login-cum-SignUp-Authentication/static/js/2.0356524d.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/Login-cum-SignUp-Authentication/static/js/2.0356524d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "634653f56002394b99f9",
+    "url": "/Login-cum-SignUp-Authentication/static/js/main.137a411f.chunk.js"
+  },
+  {
+    "revision": "49707f125cf9531ffaa7",
+    "url": "/Login-cum-SignUp-Authentication/static/js/runtime-main.21b6da74.js"
+  }
+]);
